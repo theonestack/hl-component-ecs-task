90659e1bb39f2e752496d7ff9f9c2f486a0de60f
Lohgan Nash <lohgannash@gmail.com>
2019-11-14 12:57:03 +1100

    Support for using ref with volumes/mounts (#3)
    * add tags to task definition
    
    * family property on task definition
    
    * family bug fix
    
    * barghhh
    
    * add secrets and more configurable volumes
    
    * Support for using ref with volumes/mounts
    
    * Fix rebase fail
    
    * Allow custom export name to avoid conflicts

--- ecs-task.cfndsl.rb
+++ ecs-task.cfndsl.rb
@@ -1,31 +1,36 @@
 CloudFormation do
 
-    awsvpc_enabled = false
-    if defined?(network_mode) && network_mode == 'awsvpc'
-      awsvpc_enabled = true
-    end
+    export = defined?(export_name) ? export_name : component_name
 
-  
+    task_tags = []
+    task_tags << { Key: "Name", Value: component_name }
+    task_tags << { Key: "Environment", Value: Ref("EnvironmentName") }
+    task_tags << { Key: "EnvironmentType", Value: Ref("EnvironmentType") }
+
+    tags.each do |key,value|
+      task_tags << { Key: key, Value: value }
+    end if defined? tags
+
     log_retention = 7 unless defined?(log_retention)
 
     Logs_LogGroup('LogGroup') {
       LogGroupName Ref('AWS::StackName')
       RetentionInDays "#{log_retention}"
     }
-  
-    definitions, task_volumes = Array.new(2){[]}
-  
+
+    definitions, task_volumes, secrets = Array.new(3){[]}
+
     task_definition.each do |task_name, task|
-  
+
       env_vars, mount_points, ports = Array.new(3){[]}
-  
+
       name = task.has_key?('name') ? task['name'] : task_name
-  
+
       image_repo = task.has_key?('repo') ? "#{task['repo']}/" : ''
       image_name = task.has_key?('image') ? task['image'] : task_name
       image_tag = task.has_key?('tag') ? "#{task['tag']}" : 'latest'
       image_tag = task.has_key?('tag_param') ? Ref("#{task['tag_param']}") : image_tag
-  
+
       # create main definition
       task_def =  {
         Name: name,
@@ -39,13 +44,13 @@ CloudFormation do
           }
         }
       }
-  
+
       task_def.merge!({ MemoryReservation: task['memory'] }) if task.has_key?('memory')
       task_def.merge!({ Cpu: task['cpu'] }) if task.has_key?('cpu')
-  
+
       task_def.merge!({ Ulimits: task['ulimits'] }) if task.has_key?('ulimits')
-  
-  
+
+
       if !(task['env_vars'].nil?)
         task['env_vars'].each do |name,value|
           split_value = value.to_s.split(/\${|}/)
@@ -60,42 +65,46 @@ CloudFormation do
           env_vars << { Name: name, Value: env_value}
         end
       end
-  
+
       task_def.merge!({Environment: env_vars }) if env_vars.any?
-  
+
       # add links
       if task.key?('links')
         task['links'].each do |links|
         task_def.merge!({ Links: [ links ] })
         end
       end
-  
+
       # add entrypoint
       if task.key?('entrypoint')
         task['entrypoint'].each do |entrypoint|
         task_def.merge!({ EntryPoint: entrypoint })
         end
       end
-  
+
       # By default Essential is true, switch to false if `not_essential: true`
       task_def.merge!({ Essential: false }) if task['not_essential']
-  
+
       # add docker volumes
       if task.key?('mounts')
         task['mounts'].each do |mount|
-          parts = mount.split(':')
-          mount_points << { ContainerPath: parts[0], SourceVolume: parts[1], ReadOnly: (parts[2] == 'ro' ? true : false) }
+          if mount.is_a? String 
+            parts = mount.split(':',2)
+            mount_points << { ContainerPath: FnSub(parts[0]), SourceVolume: FnSub(parts[1]), ReadOnly: (parts[2] == 'ro' ? true : false) }
+          else
+            mount_points << mount
+          end
         end
         task_def.merge!({MountPoints: mount_points })
       end
-  
+
       # volumes from
       if task.key?('volumes_from')
         task['volumes_from'].each do |source_container|
         task_def.merge!({ VolumesFrom: [ SourceContainer: source_container ] })
         end
       end
-  
+
       # add port
       if task.key?('ports')
         port_mapppings = []
@@ -108,33 +117,42 @@ CloudFormation do
         end
         task_def.merge!({PortMappings: port_mapppings})
       end
-  
+
       task_def.merge!({EntryPoint: task['entrypoint'] }) if task.key?('entrypoint')
       task_def.merge!({Command: task['command'] }) if task.key?('command')
       task_def.merge!({HealthCheck: task['healthcheck'] }) if task.key?('healthcheck')
       task_def.merge!({WorkingDirectory: task['working_dir'] }) if task.key?('working_dir')
-  
+
+      if task.key?('secrets')
+        secrets.push(*task['secrets'].values)
+        task_def.merge!({Secrets: task['secrets'].map {|k,v| { Name: k, ValueFrom: v }} })
+      end
+
       definitions << task_def
-  
+
     end if defined? task_definition
-  
+
     # add docker volumes
     if defined?(volumes)
       volumes.each do |volume|
-        parts = volume.split(':')
-        object = { Name: parts[0]}
-        object.merge!({ Host: { SourcePath: parts[1] }}) if parts[1]
+        if volume.is_a? String 
+          parts = volume.split(':')
+          object = { Name: FnSub(parts[0])}
+          object.merge!({ Host: { SourcePath: FnSub(parts[1]) }}) if parts[1]
+        else
+          object = volume
+        end
         task_volumes << object
       end
     end
-  
+
     if defined?(iam_policies)
-  
+
       policies = []
       iam_policies.each do |name,policy|
         policies << iam_policy_allow(name,policy['action'],policy['resource'] || '*')
       end
-  
+
       IAM_Role('TaskRole') do
         AssumeRolePolicyDocument ({
           Statement: [
@@ -153,47 +171,52 @@ CloudFormation do
         Path '/'
         Policies(policies)
       end
-  
+
       IAM_Role('ExecutionRole') do
         AssumeRolePolicyDocument service_role_assume_policy('ecs-tasks')
         Path '/'
         ManagedPolicyArns ["arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"]
+
+        if secrets.any?
+          resources = secrets.map {|secret| FnSub("arn:aws:ssm:${AWS::Region}:${AWS::AccountId}:parameter#{secret}")}
+          Policies [iam_policy_allow('ssm-secrets',%(ssm:GetParameters),secrets)]
+        end
+
       end
     end
-  
+
     ECS_TaskDefinition('Task') do
       ContainerDefinitions definitions
       RequiresCompatibilities defined?(task_type) ? [task_type] : ['EC2']
-  
+
       if defined?(cpu)
         Cpu cpu
       end
-  
+
       if defined?(memory)
         Memory memory
       end
-  
+
       if defined?(network_mode)
         NetworkMode network_mode
       end
-  
+
       if task_volumes.any?
         Volumes task_volumes
       end
-  
+
       if defined?(iam_policies)
         TaskRoleArn Ref('TaskRole')
         ExecutionRoleArn Ref('ExecutionRole')
       end
-  
-    end if defined? task_definition
-  
 
-  
+      Tags task_tags
+
+    end if defined? task_definition
+
     Output("EcsTaskArn") {
       Value(Ref('Task'))
-      Export FnSub("${EnvironmentName}-#{component_name}-EcsTaskArn")
+      Export FnSub("${EnvironmentName}-#{export}-EcsTaskArn")
     }
-  
+
   end
-  
